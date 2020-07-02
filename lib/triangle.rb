53cf0b1dcabe61b451f42832dce2494665afdb3f
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 20:32:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3, :equilateral, :isosceles, :scalene, :kind
+  attr_accessor :side_1, :side_2, :side_3, :equilateral, :isosceles, :scalene
   
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
@@ -9,7 +9,7 @@ class Triangle
   
   def kind
     if side_1 == side_2 && side_1 == side_3
-      self.kind = @equilateral
+      @equilateral
     end
   end
   
